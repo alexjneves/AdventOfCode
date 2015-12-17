1cb593512fb5330edba5087bbb71b5132e969a5b
Alex Neves <alexneves08@gmail.com>
2015-12-17 16:30:17 +0000

    Day 1 Solution

---
+++ AdventCalendar/AdventCalendar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Day1;
+
+namespace AdventCalendar
+{
+    internal sealed class AdventCalendar
+    {
+        private static void Main()
+        {
+            var day1Solution = new Day1Solution();
+            PrintAnswer(1, 1, day1Solution.SolvePart1());
+            PrintAnswer(1, 2, day1Solution.SolvePart2());
+        }
+
+        private static void PrintAnswer(int day, int part, int answer)
+        {
+            Console.WriteLine($"The answer to Day {day} Part {part} is {answer}");
+        }
+    }
+}
