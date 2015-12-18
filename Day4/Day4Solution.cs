d3d2fa6097f1c56e2beb889a67b35de990baae0e
Alex Neves <alexneves08@gmail.com>
2015-12-18 23:00:12 +0000

    Day 4 Part 2

--- Day4/Day4Solution.cs
+++ Day4/Day4Solution.cs
@@ -5,18 +5,21 @@ namespace Day4
     public sealed class Day4Solution : IDailyPuzzle
     {
         private const string SecretKey = "ckczppom";
-        private const string ExpectedStartingSubstring = "00000";
+        private const string Part1ExpectedStartingSubstring = "00000";
+        private const string Part2ExpectedStartingSubstring = "000000";
 
         public int SolvePart1()
         {
-            var md5SubstringHashSolver = new Md5SubstringHashSolver(SecretKey, ExpectedStartingSubstring);
+            var md5SubstringHashSolver = new Md5SubstringHashSolver(SecretKey, Part1ExpectedStartingSubstring);
 
             return md5SubstringHashSolver.Solve();
         }
 
         public int SolvePart2()
         {
-            return -1;
+            var md5SubstringHashSolver = new Md5SubstringHashSolver(SecretKey, Part2ExpectedStartingSubstring);
+
+            return md5SubstringHashSolver.Solve();
         }
     }
 }
